5062960ba8cf277fe603f9b8f3f41b27df1f94c0
Manibharathi Periasamymanjula <periasamymanjula@gmail.com>
2019-02-17 02:29:00 +0000

    Map Generator progress 20%
    needs work.

--- Codename - Slash/Loader.cs
+++ Codename - Slash/Loader.cs
@@ -8,7 +8,7 @@ using System.Xml.Serialization;
 
 namespace Codename___Slash
 {
-    class Loader
+    public class Loader
     {
         
         /// <summary>
--- Codename - Slash/MapGenerator.cs
+++ Codename - Slash/MapGenerator.cs
@@ -5,84 +5,136 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
 
 namespace Codename___Slash
 {
     // Collision indicator for each tile
-    enum TileCollision
+    // And direction of map update 
+    public enum Direction
     {
-        Left,   // Collisions only for left half of tile
-        Right,  // Collisions only for right half of tile
-        Top,    // Collisions only for top half of tile
-        Bottom, // Collisions only for bottom half of tile
-        Full,   // Collisions for entire tile
+        Left,   // only for left half of tile
+        Right,  // only for right half of tile
+        Top,    // only for top half of tile
+        Bottom, // only for bottom half of tile
+        Full,   // for entire tile
     }
 
     // Info of each tile on the grid
-    struct TileInfo
+    public struct TileInfo
     {
         public int textureIndex;
-        public TileCollision collision;
+        public Direction collision;
 
     }
     
-    struct SectionGrid
+    public struct Section
     {
-        public TileInfo[,] textureIndexMap;
+        public TileInfo[,] tilesInfo;
         
-        public SectionGrid(int gridSize)
+        public Section(int sectionSize)
         {
-            textureIndexMap = new TileInfo[gridSize, gridSize];
-
+            tilesInfo = new TileInfo[sectionSize, sectionSize];
+            // TODO : 
         }
 
     }
 
-    class Map
+    public class Map
     {
+        public Section[,] MapSections { get; }
+        public Dictionary<int, string> IndexToTextureFile { get; }
+
         private int mapNumber;
         private int mapSize;
-        private SectionGrid[,] mapSections;
-        private Dictionary<int, Texture2D> currentIndexToTexDic;
 
         public Map(int mapSize)
         {
             // Assign parameter values
             this.mapSize = mapSize;
-            mapSections = new SectionGrid[mapSize, mapSize];
-            currentIndexToTexDic = new Dictionary<int, Texture2D>();
+            MapSections = new Section[mapSize, mapSize];
+            IndexToTextureFile = new Dictionary<int, string>();
         }
 
 
     }
 
 
-    class MapGenerator
+    public class MapGenerator
     {
         private Map currentMap;
         private int currentMapNumber;
-        
+        private Vector2Int currentHeroPos;
+        private Deque<Deque<Section>> currentChunk;
+        private int renderChunkSize;
+        private Dictionary<int, Texture2D> IndexToTextureLoaded; // Indexes of the tiles currently loaded from content manager (Used to check to not load again)
+
+        private List<Section> currentGridsDisplayed;
+
         private ContentManager content;
 
         public MapGenerator(IServiceProvider serivceProvider)
         {
-            content = new ContentManager(serivceProvider);
+            content = new ContentManager(serivceProvider, "Content/Maps");
         }
 
         
-        public void InitialiseNewMap(int MapNumber, int mapSize)
+        public void InitialiseNewMap(int MapNumber, int mapSize, Vector2Int heroPos, int renderChunkSize)
         {
             // Reinitialise values when creating a new map, or newly assign if first time
             currentMapNumber = MapNumber;
             currentMap = new Map(mapSize);
+            currentGridsDisplayed = new List<Section>();
+            currentChunk = null;
+            currentHeroPos = heroPos;
+            this.renderChunkSize = renderChunkSize;
 
             // Read XML map data onto the new Map object, based on mapNumber
-            Loader.ReadXML(string.Format("Content/Levels/{0}.txt", MapNumber), ref currentMap);
+            // Should update Section Values with TilesInfo
+            Loader.ReadXML(string.Format("Content/Maps/{0}.txt", MapNumber), ref currentMap);
         }
 
-        public void UpdateMapDisplay()
+        public void UpdateMapDisplay(Direction dir)
         {
+            if (currentChunk != null)
+            {
 
+            } else
+            {
+                // Create new chunk, then for each deque, add a deque with the relevant Sections based on the heroPos
+                currentChunk = new Deque<Deque<Section>>();
+                for (int i = 0; i < 3; i++)
+                {
+                    currentChunk.Add(new Deque<Section> { currentMap.MapSections[currentHeroPos.X - 1, currentHeroPos.Y + (i-1)],
+                                                        currentMap.MapSections[currentHeroPos.X, currentHeroPos.Y + (i-1)],
+                                                        currentMap.MapSections[currentHeroPos.X + 1, currentHeroPos.Y + (i-1)] });
+                }
+
+                foreach (Deque<Section> s in currentChunk) 
+                {
+                    foreach (Section s2 in s)
+                    {
+                        int l1 = s2.tilesInfo.GetLength(0);
+                        int l2 = s2.tilesInfo.GetLength(1);
+                        for (int i=0; i < l1; i++)
+                        {
+                            for(int j=0; j < l2; j++)
+                            {
+                                int index = s2.tilesInfo[i, j].textureIndex;
+
+                                if (!IndexToTextureLoaded.ContainsKey(index))
+                                    IndexToTextureLoaded[index] = content.Load<Texture2D>(string.Format("{0}/Tiles", currentMapNumber));
+                                
+                                // TODO : Add collision for tiles
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            // Update currentHeroPos based on dir
+            
 
         }
 
